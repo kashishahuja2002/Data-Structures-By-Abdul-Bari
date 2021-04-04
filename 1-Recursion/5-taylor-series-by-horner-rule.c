ab44e89b147c6c6c242f4cad09ab784458cc3ec9
kashishahuja2002 <kashishahuja2002@gmail.com>
2021-04-04 20:10:31 +0530

    recursion Q5

---
+++ 1-Recursion/5-taylor-series-by-horner-rule.c
@@ -0,0 +1,29 @@
+//  Kashish Ahuja
+//  Program for taylor's series by Horner's rule using recursion
+
+#include<stdio.h>
+#include<conio.h>
+
+//  using static variable in recursion
+double taylor(int x, int n)
+{
+    static int ele=1;
+    if(n>0)
+    {
+        ele =1+ (x/n)*ele;
+        return taylor(x,n-1);
+    }
+    else
+        return ele;
+}    
+
+void main()
+{
+    int x, n;
+    printf("\nEnter value of x: ");
+    scanf("%d",&x);
+    printf("Enter number of terms: ");
+    scanf("%d",&n);
+    printf("\nBy Horner's rule: Sum of tylor series for %d upto %d terms is %lf ",x,n,taylor(x,n));
+    getch();
+}
